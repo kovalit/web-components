bf817519035db2ef957479233cb2c4bef899d3ed
Aleksey Kovalenko <kovalit@mail.ru>
2018-03-20 18:58:03 +0300

    fix: post styles

--- popup-post/index.js
+++ popup-post/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Popup from 'components/popup';
+import './post.scss';
 import './popup-post.scss';
 
 
@@ -29,20 +30,14 @@ class PopupPost extends Component {
     render() {
 	return (
 	    <div className="article">
-
-		<div className="article-container">
-		    <div className="article-top clearfix">
-			<a href="#" className="article-logo">
-			    <img src="/img/logo@2x.png" alt=""/>
-			</a>
-			<a href="#" className="grey-btn btn">Все статьи</a>
-		    </div>
-		    <div className="article-txt">
-			<h2>{this.state.title}</h2>
-			<div dangerouslySetInnerHTML={{__html: this.state.content}} />
-		    </div>
+		<div className="article-top clearfix">
+		    <a href="#" className="article-logo">
+			<img src="/img/logo@2x.png" alt=""/>
+		    </a>
+		    <a href="#" className="grey-btn btn">Все статьи</a>
 		</div>
-
+		<h2>{this.state.title}</h2>
+		<div className="article-txt" dangerouslySetInnerHTML={{__html: this.state.content}} />
 		<a href="#" className="all-link btn">Все посты</a>
 	    </div>
 	);
