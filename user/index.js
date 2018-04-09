dc0e6ade79b2fcdc36320f63d29859ebb84af7d9
Aleksey Kovalenko <kovalit@mail.ru>
2018-04-09 18:06:42 +0300

    added: user

---
+++ user/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+
+const User = ({user}) => {
+    return (
+	<div className="user">
+	<div className="table-cell">
+	    <div className="avatar32" style={{backgroundImage: `url(${user.main_image})`}}></div>
+	</div>
+	<div className="table-cell">
+	    <div className="user">{user.name}</div>
+	</div>
+	</div>
+    );
+};
+
+export default User;
