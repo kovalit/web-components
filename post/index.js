e818e87940f45805c151f77fa42ca411136104a6
kovalit <kovalit@mail.ru>
2018-04-24 17:09:06 +0300

    fix: post

---
+++ post/index.js
@@ -0,0 +1,97 @@
+import React, { Component } from 'react';
+import "./post.scss"
+
+//modules
+import api from 'api';
+import request from 'request';
+import { mountComponent } from './mount-component';	
+
+//component
+import User from 'components/user';
+import ScoreFull from 'components/score-full';
+
+class Post extends Component {
+
+    constructor(props) {
+        super(props);	
+	
+	this.state = {
+	    post: null
+	};	
+    }
+    
+    
+    componentDidMount() {
+	request(api.posts.findByAlias, {id: this.props.id})
+	    .then(body => {
+		this.setState({
+		    post: body.post,
+		});
+	    });
+    }
+    
+    
+    componentDidUpdate() {
+	if (!this.state.post) {
+	    return;
+	}
+
+	let {scores} = this.state.post.metadata;
+
+	if (scores.length > 0) {
+	    mountComponent('Score', ScoreFull, scores);   
+	}
+	 
+	//mountComponent('Gallery', Gallery, passProps); 
+	//mountComponent('Top', Top, passProps); 
+    }
+
+
+//    onEdit() {
+//	if (this.props.isPopup) {
+//	    this.props.hidePopup("post");
+//	    this.props.showPopup("post-edit", {id: this.state.post.id});
+//	}
+//	else {
+//	    location.replace("/posts/edit/" + this.state.post.id);
+//	}
+//    }
+
+   
+
+    render() {
+	
+	const { post } = this.state;
+	
+        return (
+	    <div className="post-wrapper">
+		<div className="post-close" onClick={this.props.onClose}></div>
+		<div className="post" onClick={this.focus}>
+		    <If condition={post}>    
+			<div className="post-header">
+			    <div className="float-left">
+				<User user={post.author} />
+			    </div>		 
+			    <If condition={post.author.id === this.props.me.id}>
+				<div className="float-right"> 
+				    <div className="post-btn" onClick={this.props.onEdit}>Редактировать</div> 
+				</div>
+			    </If>
+
+			    <div className="clear-fix"> </div>
+
+			</div>
+
+
+			<div className="post-title">{post.title}</div>
+			<div dangerouslySetInnerHTML={{__html: decodeURI(post.content) }} /> 
+		    </If>
+		</div>
+	    </div>
+	    
+	);
+    }
+}
+
+export default Post;
+    
---
+++ post/mount-component.js
@@ -0,0 +1,225 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import camelcase from 'camelcase';
+
+var DATA_ATTR_REGEX = /^([\w-_]*)/i;
+var components = [];
+
+/**
+ * Resolves a named attribute from an Element.
+ *
+ * @private
+ * @param {Element} element Element to get the attribute from
+ * @param {String} name Name of the attribute (key)
+ * @param {*} [defaultValue] Optional value returned if the attribute dones nt exists.
+ *
+ * @returns {*} The attribute's value or defaultValue
+ */
+function getNamedAttribute(element, name, defaultValue) {
+
+    if (!element || !element.attributes || !element.attributes.getNamedItem) {
+        return defaultValue;
+    }
+
+    return element.attributes.getNamedItem(name) ?
+           element.attributes.getNamedItem(name).nodeValue :
+           defaultValue;
+}
+
+/**
+ * loads JSON from a given element
+ *
+ * @param el
+ * @returns {*}
+ */
+function loadJson(el) {
+
+    if (!el ||
+        el.tagName.toLowerCase() !== 'script' ||
+        getNamedAttribute(el, 'type') !== 'application/json'
+    ) {
+        return null;
+    }
+
+    var result = {};
+
+    // Get the key-name from `data-name`, `name`, default is "data"
+    var key = camelcase(getNamedAttribute(el, 'data-name', 'data'));
+
+    try {
+        var content = el.innerHTML;
+        result[key] = JSON.parse(content);
+    } catch(e) {}
+
+    return result;
+
+}
+
+/**
+ * Gets props for element
+ *
+ * @private
+ * @param {Element} el Element to get props for
+ * @returns {object}
+ */
+function getProps(el) {
+
+    // Try if the mounting-node is a script/json tag ...
+    var result = loadJson(el);
+    // ... if not, examine the children.
+    if (result === null) {
+
+        result = {};
+
+        var children = el.children;
+        for (var i = 0; i < children.length; i++) {
+
+            var json = loadJson(children[i] || {});
+            Object.assign(result, json);
+        }
+
+    }
+
+    if (!el.hasAttributes()) {
+        return result;
+    }
+
+    var attrs = el.attributes;
+    for (var j = attrs.length - 1; j >= 0; j--) {
+
+        var match = DATA_ATTR_REGEX.exec(attrs[j].name);
+        if (!match || match.length < 2) {
+            continue;
+        }
+
+        var propName = camelcase(match[1]);
+        var value = decodeURI(attrs[j].value);
+
+        result[propName] = value;
+
+    }
+
+    return result;
+
+}
+
+/**
+ * Mounts React- or Pure-JS-Components for all elements matching the selector.
+ * Any data-attributes will be passed as props, prop-names are converted to camelcase,
+ * with the 'data-' part stripped off (eg. "data-base-path" will become "basePath").
+ * Tries to parse JSON for props, passes values as objects on success, as string otherwise.
+ * Provides a references to the mounted node as `this.props.mountNode`.
+ *
+ * Will also instantiate non-React-Components, by passing the mounted node and props to the varructor.
+ *
+ * @param {string}                 selector  Selector to convert into react-components
+ * @param {React.Component|string|function} component The (un-instantiated) React-Component, it's name or any
+ *                                                    non-react function(node: Element, props: Object)
+ * @param {object} [properties] Optional props to be passed to a component.
+ *                              Tag-bound props will take precedence.
+ * @return {function|array|null} The instance, if one result, an array of instances for multiple results, null for no results.
+ */
+function mountComponent(selector, component, properties) {
+
+    var elements = document.querySelectorAll(selector);
+
+    if (!elements ||
+        !elements.length ||
+        !component ||
+        !component.prototype
+    ) {
+        return null;
+    }
+
+    var result = [];
+
+    for (var i = 0, l = elements.length; i < l; i++) {
+
+        var el = elements[i];
+
+        if (!el) {
+            continue;
+        }
+	
+	
+        var props = getProps(el);
+	    
+        if (typeof properties !== 'undefined') {
+
+	    let filterProps = properties.reduce((item, cur) => {
+		if (cur.id === props.id) {
+		    return cur;  
+		} 
+		return item;  
+	    }, {});
+
+            props = Object.assign({}, filterProps);
+        }
+        props.mountNode = el;
+
+        // Allows mounting non-react components
+        if (!React || (!!React && !component.prototype.isReactComponent)) {
+
+            /*eslint no-new: 0*/
+            result.push(new component(props, el));
+
+            continue;
+        }
+
+        if (!React) {
+            continue;
+        }
+
+        var reactElement = React.createElement(component, props);
+        ReactDOM.render(
+            reactElement,
+            el
+        );
+
+        result.push(reactElement);
+
+    }
+
+    if (!result.length) {
+        return null;
+    }
+
+    if (result.length === 1) {
+        return result[0];
+    }
+
+    return result;
+}
+
+
+/**
+ * Registers a component globally to be mounted.
+ *
+ * @param {string} selector
+ * @param {function} component
+ */
+function registerComponent(selector, component) {
+
+    var item = {};
+    item.selector = selector;
+    item.component = component;
+
+    components.push(item);
+}
+
+/**
+ * Mounts all registered components.
+ */
+function mountAll() {
+
+    components.forEach(function(item) {
+        mountComponent(item.selector, item.component);
+    });
+}
+
+module.exports = {
+    mountComponent: mountComponent,
+    registerComponent: registerComponent,
+    mountAll: mountAll,
+};
