0606e6257c897f2e22ec3c3056cf9cc05d26bf19
kovalit <kovalit@mail.ru>
2018-05-12 10:28:58 +0300

    feat (post-prewiew): added images

--- post-preview/index.js
+++ post-preview/index.js
@@ -4,6 +4,7 @@ import './post-preview.scss'
 //Modules
 import helpers from 'helpers';
 import rand from 'random-seed';
+import classnames from 'classnames';
 
 // Components
 import User from 'components/user';
@@ -11,22 +12,32 @@ import User from 'components/user';
 
 const PostPreview = {
     Row: ({post, segment, onOpen}) => {	
+	let withImages = post.preambleImages.length > 0;
 	let gen = rand.create(post.id);
 	let index = gen(24);
-	let color = helpers.color(index);
-	
+	let color = withImages ? "#fff" : helpers.color(index);
+
 	return (
-	     <div className="posts-item" >
+	     <div className="post-prewiew-row ppr" onClick={onOpen}>
 		<User user={post.author} size={48} />
 		<If condition={segment === "me"}>
 		    <div>{post.published ? "Опубликован" : "Не опубликован" }</div>
 		</If>
-		<div className="posts-item-content" style={{backgroundColor: color}} onClick={onOpen} >
-		    <div className="posts-item__title">{post.title}</div>
-		    <If condition={post.preamble !== ""} >
-			<div className="posts-item__preamble">{post.preamble}</div>
+		<If condition={withImages}>
+		    <div className="ppr-images">
+			<For each="image" index="index" of={ post.preambleImages }>
+			    <div className="ppr__image" style={helpers.imgStyle(image.hash)} />
+			</For>
+		    </div>
+		</If>
+		<div className={classnames({"ppr-content": true, "ppr-content-with-img": withImages})} 
+		    style={{backgroundColor: color}} >
+			
+ 		    <div className="ppr__title">{post.title}</div>
+		    <If condition={post.preamble !== ""}>
+			<div className="ppr__preamble">{post.preamble}</div>
 		    </If>			    
-		    <div className="posts-item__date">{helpers.dateFormat(post.createdAt, false)}</div>
+		    <div className="ppr__date">{helpers.dateFormat(post.createdAt, false)}</div>
 		</div>
 	    </div>
 	);
